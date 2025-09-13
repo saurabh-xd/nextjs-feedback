25e9368d7d42b581869ebcaef7446f287b11979b
saurabh garkoti <saurabhgarkotiii@gmail.com>
2025-09-13 12:29:46 +0530

    add: db connect and email send functionality

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,29 @@
+import { resend } from "@/lib/resend";
+
+import VerificationEmail from "../../emails/verificationEmail";
+import { ApiResponse } from "@/types/ApiResponse";
+import { success } from "zod";
+
+export async function sendVerificationEmail(
+    email: string,
+    username: string,
+    verifyCode: string
+): Promise<ApiResponse>{
+        try {
+            await resend.emails.send({
+  from: 'onboarding@resend.dev',
+  to: email, 
+  subject: 'Nextjs-feedback || Verification code',
+  react: VerificationEmail({username, otp:verifyCode}),
+});
+
+            return {success: true, message: 'verification email send successfully'}
+
+            
+        } catch (emailError) {
+            console.error("Error sending verification email",emailError);
+            return {success: false, message: 'failed to send verification email'}
+            
+        }
+}
+
