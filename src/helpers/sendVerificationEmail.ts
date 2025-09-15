8178caeec71106c729f54917e1c2ee2b914ae4a4
saurabh garkoti <saurabhgarkotiii@gmail.com>
2025-09-15 19:59:51 +0530

    email bug fixed

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -12,7 +12,7 @@ export async function sendVerificationEmail(
       from: 'onboarding@resend.dev',
       to: [email],
       subject: 'Mystery Message Verification Code',
-      react: VerificationEmail({ username, otp: verifyCode }),
+      react: VerificationEmail({ username: username, otp: verifyCode }),
     });
     return { success: true, message: 'Verification email sent successfully.' };
   } catch (emailError) {
